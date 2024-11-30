a7dff3055e20b6c244dc9c1f4cd69782be870d9c
Tufahel Ahmed <tufahel96@gmail.com>
2024-11-30 15:30:21 +0600

    Basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>E-commerce Store</h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
